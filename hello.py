2488ba3772e982200a5f3632046aba2f6ce4704b
fq1995 <1152237929@qq.com>
2017-12-14 17:16:32 +0800

    if else while

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# https://www.liaoxuefeng.com/
+
 # name = input("please enter your name: ")
 # print(name)
 
@@ -8,8 +10,8 @@
 # number2 = input("number2: ")
 # print(number1 ,"*", number2, "=" , int(number1) * int(number2))
 
+'''
 a=int(input('age:'))
-
 if 18<=a<=59:
     print('welcome')
 elif 0 <= a <=6:
@@ -18,3 +20,80 @@ elif a>=60:
     print('poor single dog')
 else:
     print(18-a,'years later come')
+'''
+	
+	
+#s1 = 72
+#s2 = 85
+#r = (s2 - s1)/s1 *100
+#print('%.2f%%'%r) 
+#print('{0:.2f}%'.format(r))
+
+'''
+classmates = ['Mi','Xi','Do']
+classmates.pop(1)
+classmates[0] = 'So'
+print(classmates)
+'''
+
+
+'''
+height = float(input('身高（米）'))
+weight = float(input('体重（Kg）'))
+bmi = weight/height/height
+print ('%.1f' %bmi)
+if bmi<18.5:
+    print('过轻')
+elif bmi<25:
+    print('正常')
+elif bmi<28:
+    print('过重')
+elif bmi<32:
+    print('肥胖')
+else:
+    print('严重肥胖')
+
+'''
+
+'''
+sum = 0
+for x in range(101):
+	sum = sum + x
+print(sum)
+'''
+
+'''
+L = ['Bart', 'Lisa', 'Adam']
+for x in L:
+    print(x)
+'''
+
+n = 0qqqqqqqqqqqqqqqqqqqq
+while n <= 100:
+    n = n + 1
+    if n > 10:
+	    break
+    if n % 2 == 0:
+        continue
+    print(n)
+    
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
